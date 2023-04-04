202371e8d8bb8c549bdde438345e5ba4e089ac36
Михаил Трухин <meatrue@gmail.com>
2023-04-04 15:17:07 +0300

    Меняет BrowserRouter на HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './components/app/app';
 
@@ -11,10 +11,10 @@ import { store } from './store';
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
